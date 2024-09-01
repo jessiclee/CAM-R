b33adfd53a1e193496ce14bb4d367ad2eed1af4f
bath-salt5 <zhiyi.kwek.2021@scis.smu.edu.sg>
2024-09-01 16:27:24 +0800

    added stuff for training

--- models/object-detection/yolo-8/test.py
+++ models/object-detection/yolo-8/test.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt") 
-metrics = model.val(data="C:/Users/User/CAM-R/models/object-detection/yolo_data/yolo_data.yaml", batch=32, iou=0.5, device="cpu")
+metrics = model.val(data="C:/Users/User/CAM-R/models/object-detection/yolo_data/yolo_data.yaml", batch=32, iou=0.5, device="cpu", save_json=True, plots=True)
 # print(metrics)
---
+++ models/object-detection/yolo-8/train.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from YAML
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.yaml").load("yolov8n.pt")  # build from YAML and transfer weights
+
+# Train the model
+results = model.train(data="C:/Users/User/CAM-R/models/object-detection/yolo_data/yolo_data.yaml", epochs=5, batch=32)
