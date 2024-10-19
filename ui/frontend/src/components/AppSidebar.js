80c7062fc88cbed70fcaca1c41614a48a1d1cb45
Jess <jesstohxl@gmail.com>
2024-10-19 19:02:37 +0800

    removed icon from sidebar

--- ui/frontend/src/components/AppSidebar.js
+++ ui/frontend/src/components/AppSidebar.js
@@ -37,8 +37,8 @@ const AppSidebar = () => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          {/* <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} /> */}
+          {/* <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} /> */}
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
